2cec6ee8eac7853b2e7b06f5a6573dcc4a52bfc4
antauth <smith@antoinetteauthedna.com>
2013-12-09 00:46:27 -0600

    Somehow I missed these css files on last commit

--- js/playlist.js
+++ js/playlist.js
@@ -5,3 +5,7 @@
  * Time: 9:36 PM
  * To change this template use File | Settings | File Templates.
  */
+var song_title = ["The Next Movement", "The 6th Sense", "One (Piano)"];
+var song_artist = ["The Roots", "Common", "Unclassicalpiano"]
+var playlist = ["music/03 - The Next Movement [Explicit].mp3", "music/09 - The 6th Sense.mp3", "music/01 - Metallica - One (Piano).mp3"];
+var album_cover = ["images/roots-things-fall-apart.jpg", "images/common-water-for-chocolate.jpg", "images/one-metallica.jpg"];
